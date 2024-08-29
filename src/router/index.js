ff99900582bf493380c3f8c3771d893c5eb83e0b
Kinleoapple <kinleoapple2460@gmail.com>
2024-08-29 18:00:47 +1000

    Updated

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,6 @@ import HomeView from "@/views/HomeView.vue";
 import LoginView from "@/views/LoginView.vue";
 import RegisterView from "@/views/RegisterView.vue";
 import ReservationView from "@/views/ReservationView.vue";
-import ProfileView from "@/views/ProfileView.vue";
 
 const isLogin = () => {
     const accounts = JSON.parse(localStorage.getItem("accounts") || "[]");
@@ -26,18 +25,6 @@ const routes = [
         path: '/reservation',
         name: 'Reservation',
         component: ReservationView,
-        beforeEnter: (to, from, next) => {
-            if (isLogin()) {
-                next();
-            } else {
-                next({ name: 'Login' });
-            }
-        }
-    },
-    {
-        path: '/profile',
-        name: 'Profile',
-        component: ProfileView,
         beforeEnter: (to, from, next) => {
             if (isLogin()) {
                 next();
