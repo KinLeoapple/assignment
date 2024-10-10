3fe5acc455295bf773ae85645aa92bc374b75edb
Kinleoapple2460 <kinleoapple2460@gmail.com>
2024-10-11 10:02:58 +1100

    Updated email.

--- src/assets/js/email.js
+++ src/assets/js/email.js
@@ -2,15 +2,15 @@ import emailjs from "@emailjs/browser";
 
 export const sendEmail = (to_name, message, to_email) => {
     emailjs.init("okrql5pOHtOF7E81I");
-    emailjs.send("service_6b1kjew", "template_k9c5yf8", {
-        to_name: to_name,
+    emailjs.send("service_6b1kjew", "template_k5c9yf8", {
         from_name: "Haojun Huang",
+        to_name: to_name,
         message: message,
         to_email: to_email,
     })
-        .then(function(response) {
+        .then(function (response) {
             console.log("Success", response.status, response.text);
-        }, function(error) {
+        }, function (error) {
             console.log("Error", error);
         });
 }
