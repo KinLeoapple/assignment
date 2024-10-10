d50cd33e7909b535b16db72e10256ca3d136bba1
Kinleoapple2460 <kinleoapple2460@gmail.com>
2024-10-11 00:40:16 +1100

    Updated Bulk Email.

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -7,6 +7,7 @@ import {CanvasRenderer} from 'echarts/renderers';
 import {nextTick, onMounted, ref} from "vue";
 import {collection, getDocs, query, where} from "firebase/firestore";
 import db from "@/firebase/db.js";
+import {sendEmail} from "@/assets/js/email.js";
 
 const isLogin = ref(false);
 
@@ -87,7 +88,10 @@ function drawPieChart(data) {
   });
 }
 
+const users = ref([]);
+
 const userCount = ref(0);
+
 async function getUserData() {
   const q1 = query(collection(db, "users"), where("role", "==", "2"));
   const psychotherapists = await getDocs(q1);
@@ -96,10 +100,24 @@ async function getUserData() {
   const members = await getDocs(q2);
 
   userCount.value = psychotherapists.size + members.size;
+  psychotherapists.forEach(psychotherapist => {
+    users.value.push(psychotherapist.data());
+  });
+  members.forEach(member => {
+    users.value.push(member.data());
+  });
   return [
     {value: psychotherapists.size, name: 'Psychotherapist'},
     {value: members.size, name: 'Member'},
   ]
+}
+
+const emailMsg = ref("");
+const sendBulkEmail = () => {
+    users.value.forEach((user) => {
+      sendEmail(user.username, emailMsg.value, user.email);
+      emailMsg.value = "";
+    });
 }
 
 onMounted(() => {
@@ -132,10 +150,30 @@ onMounted(() => {
   </form>
 
   <!-- After Login -->
-  <div v-if="isLogin" class="container-fluid">
-    <div class="row justify-content-start align-items-center mt-3">
-      <h3>Total user count: {{ userCount }}</h3>
+  <div v-if="isLogin" class="container-fluid d-inline-flex justify-content-start align-items-start gap-3">
+    <!-- Show User List -->
+    <div>
+      <div v-for="user in users" class="mt-2">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">{{ user.username }}</h5>
+            <p class="card-text">Email: {{ user.email }}</p>
+            <p class="card-text">Role: {{ user.role === "1" ? "Member" : "Psychotherapist" }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Show User Chart -->
+    <div class="row justify-content-center align-items-center mt-3">
+      <h3 class="text-center">Total user count: {{ userCount }}</h3>
       <div id="pieChart" style="width: 400px;height:400px;"></div>
+    </div>
+    <div class="mt-3">
+      <h2>Bulk Email Message</h2>
+      <div class="form-outline">
+        <textarea class="form-control" id="BulkEmailTextArea" rows="4" v-model="emailMsg"></textarea>
+      </div>
+      <button @click="sendBulkEmail" :disabled="emailMsg.length <= 0" class="btn btn-primary mt-1">Send Bulk Email</button>
     </div>
   </div>
 </template>
